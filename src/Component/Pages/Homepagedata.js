896fcdf8ba0a6fd736f8dd542a22b5830d1a6590
ritikk786 <kumarritik9540@gmail.com>
2023-05-07 23:21:16 +0530

    fetch list on page load

--- src/Component/Pages/Homepagedata.js
+++ src/Component/Pages/Homepagedata.js
@@ -1,86 +1,90 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Button } from 'react-bootstrap';
 import Fetchdata from './Fetchdata'
 
 
-const Homepagedata =  () => {
-  const [movies, setMovies]=useState([]);
-  const [isloading, setIsloading]=useState(false)
-  const [error, setError]=useState(null)
-  const [stopfetching, setStopfetching]=useState(false)
-  
- async function fetchhandler(){
-  try{
-    
-    setIsloading(true)
-    const response = await fetch('https://swapi.dev/api/films');
-  
-    if(!response.ok){
-      throw new Error('something went wrong  ....Retrying')
+const Homepagedata = () => {
+  const [movies, setMovies] = useState([]);
+  const [isloading, setIsloading] = useState(false)
+  const [error, setError] = useState(null)
+  const [stopfetching, setStopfetching] = useState(false)
 
+  const fetchhandler = useCallback(async () => {
+    try {
+      setIsloading(true)
+
+      const response = await fetch('https://swapi.dev/api/films');
+
+      if (!response.ok) {
+        throw new Error('something went wrong  ....Retrying')
+      }
+
+      console.log('response', response)
+
+      const data = await response.json()
+
+      console.log('data', data)
+
+      const transformeddata = data.results.map((item) => {
+        return {
+          id: item.episode_id,
+          title: item.title,
+          director: item.director,
+          date: item.release_date,
+        }
+      })
+
+      console.log('transformed', transformeddata)
+
+      setMovies(transformeddata)
+      console.log('nothing')
     }
-    
-    console.log('response',response)
 
-    const data = await response.json()
-      console.log('data',data)
-        const transformeddata = data.results.map((item)=>{
-          return {
-            id:item.episode_id,
-            title:item.title,
-            director:item.director,
-            date:item.release_date,
-  
-          }
-  
-        })
-        console.log('transformed', transformeddata)
-        setMovies(transformeddata)
-        console.log('nothing')
-      }
-      catch(error){
-        setError(error.message)
-      }
-      setIsloading(false)
-      
-  }
-  useEffect(()=>{
+    catch (error) {
+      setError(error.message)
+    }
+
+    setIsloading(false)
+  },[])
+
+  useEffect(() => {
     let timer;
-    if(error){
+    if (error) {
 
       setStopfetching(true)
-       timer = setTimeout(() => {
+      timer = setTimeout(() => {
         fetchhandler()
         setError(null)
       }, 5000);
     }
-    return ()=>{
+    return () => {
       clearTimeout(timer)
     }
-  },[error])
+  }, [error])
 
-  const stopfetchinghandler=()=>{
+  const stopfetchinghandler = () => {
     setStopfetching(false)
     setError(null)
-   
+
   }
-  useEffect(()=>{
+  useEffect(() => {
     fetchhandler()
-  },[])
+  }, [fetchhandler])
+
   return (
     <>
- 
-
-{/* <Button onClick={fetchhandler}> Show Movies</Button> */}
 
 
-{console.log('component')}
-    {stopfetching && <Button onClick={stopfetchinghandler} className='btn-danger'>Cancel</Button> }
-    
-    {isloading && <p>Loading...</p>}
-    {error && !isloading && <p>{error}</p> }
-    {!isloading && <Fetchdata movies={movies}/> }
-    
+      {/* <Button onClick={fetchhandler}> Show Movies</Button> */}
+
+
+
+      {stopfetching && <Button onClick={stopfetchinghandler} className='btn-danger'>Cancel</Button>}
+
+      {isloading && <p>Loading...</p>}
+      {error && !isloading && <p>{error}</p>}
+      {!isloading && <Fetchdata movies={movies} />}
+
     </>
   )
 }
